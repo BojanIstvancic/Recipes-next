8e8bc6239a7f61180f03289309fb4e0fbd627e38
Bojan Ištvančić <bojan.istvancic@p3-group.com>
2024-10-18 14:56:50 +0200

    remove redundant code

--- lib/recipeISR.ts
+++
@@ -1,7 +0,0 @@
-export const getRecipes = async () => {
-  const response = await fetch("https://dummyjson.com/recipes", {
-    next: { revalidate: 3600 },
-  });
-  const data = await response.json();
-  return data.recipes;
-};
